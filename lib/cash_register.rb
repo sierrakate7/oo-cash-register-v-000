88a177817bfd2fb432010b387de6fb927d5cc58e
Sierra McCahon <smitche4@mail.umw.edu>
2018-08-30 22:09:53 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,19 +1,13 @@
 class CashRegister
   attr_accessor :discount, :total
   
-  def initialize(discount = nil)
-    self.total = 0
-    self.discount = discount
-    self.items = []
-  end
-  
-  def add_item(title, price, quantity = 1)
-    self.total += price * quantity
-    quantity.times { self.items << title }
-    self.last_transaction = [title, price, quantity]
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @cart = []
   end
   
 end 
-  
 
   

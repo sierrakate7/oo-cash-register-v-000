7cc36b4645b9e317084d1b1ec79cba90ab026156
Sierra McCahon <smitche4@mail.umw.edu>
2018-08-30 22:06:39 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,13 @@
 class CashRegister
   attr_accessor :discount, :total
   
+   def initialize(discount = nil)
+    self.total = 0
+    self.discount = discount
+    self.items = []
+  end
+  
+  
+  
 
   

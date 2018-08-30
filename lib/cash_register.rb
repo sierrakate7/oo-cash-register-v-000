c05d9498e6dcf42501a404515080d60ab79a91b1
Sierra McCahon <smitche4@mail.umw.edu>
2018-08-30 22:20:14 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,11 +23,5 @@ class CashRegister
     self.total == 0 ? "There is no discount to apply." : "After the discount, the total comes to $#{self.total = self.total * 8 / 10 }."
   end
   
-def void_last_transaction
-    self.total -= self.last_transaction[1] * self.last_transaction[2]
-    self.last_transaction[2].times do
-      self.items.delete_at(self.items.index(self.last_transaction[0]) || self.items.count)
-    end
-  end
-  
+
 end 
